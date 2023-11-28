5cbe3448419a516c4f465fae851aa6a6f8e12daf
phamtiencong <pcong1014@gamil.com>
2023-11-28 13:35:47 +0700

    baitap

---
+++ chuvi.js
@@ -0,0 +1,6 @@
+let R = +prompt("nhap R");
+const pi = 3.14;
+let chuvihinhtron = 2*R*pi;
+document.write("chu vi hinh tron la" + chuvihinhtron + "<br>");
+let dientichhinhtron = R^2*pi;
+document.write("dien tich hinh tron la " + dientichhinhtron);
---
+++ nhungjava.js
@@ -0,0 +1,6 @@
+let a = +prompt ("nhap a") ;
+let b = +prompt ("nhap b") ;
+let c = +prompt ("nhap c") ;
+let sum = a+b+c ;
+let avg = sum/3;
+document.write(avg);
